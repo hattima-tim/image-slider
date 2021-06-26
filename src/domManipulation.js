b09c847742e6772c8a0ae7584461cab53cfe83bb
ridwan <irfanridwan20@gmail.com>
2021-06-26 21:21:59 +0600

    added functions for showing next and previous image when user clicks navigation buttons and navigation dots,also added code for changing the color of navigation dots while the user is navigating through images

---
+++ src/domManipulation.js
@@ -0,0 +1,71 @@
+const images = document.querySelectorAll('img');
+const circles = document.querySelectorAll('.dot');
+circles[0].style.backgroundColor = 'red';
+let a = 0;
+
+const showNextImage = () => {
+  if (a === 2) {
+    a = -1;
+  }
+  a += 1;
+
+  switch (a) {
+    case 0:
+      images[2].classList.replace('visible', 'hidden');
+      images[0].classList.replace('hidden', 'visible');
+      circles[2].style.backgroundColor = '#bbb';
+      circles[0].style.backgroundColor = 'red';
+      break;
+    case 2:
+      images[1].classList.replace('visible', 'hidden');
+      images[2].classList.replace('hidden', 'visible');
+      circles[1].style.backgroundColor = '#bbb';
+      circles[2].style.backgroundColor = 'red';
+      break;
+    default:
+      images[a - 1].classList.replace('visible', 'hidden');
+      images[a].classList.replace('hidden', 'visible');
+      circles[a - 1].style.backgroundColor = '#bbb';
+      circles[a].style.backgroundColor = 'red';
+  }
+};
+
+const showPreviousImage = () => {
+  a -= 1;
+  if (a < 0) {
+    a = 2;
+  }
+  switch (a) {
+    case 0:
+      images[1].classList.replace('visible', 'hidden');
+      images[0].classList.replace('hidden', 'visible');
+      circles[1].style.backgroundColor = '#bbb';
+      circles[0].style.backgroundColor = 'red';
+      break;
+    case 2:
+      images[0].classList.replace('visible', 'hidden');
+      images[2].classList.replace('hidden', 'visible');
+      circles[0].style.backgroundColor = '#bbb';
+      circles[2].style.backgroundColor = 'red';
+      break;
+    default:
+      images[a + 1].classList.replace('visible', 'hidden');
+      images[a].classList.replace('hidden', 'visible');
+      circles[a + 1].style.backgroundColor = '#bbb';
+      circles[a].style.backgroundColor = 'red';
+  }
+};
+
+const linkCircleWithClickedImage = (e) => {
+  images[a].classList.replace('visible', 'hidden');
+  images[e.target.dataset.index].classList.replace('hidden', 'visible');
+  circles[a].style.backgroundColor = '#bbb';
+  e.target.style.backgroundColor = 'red';
+  a = Number(e.target.dataset.index);
+};
+
+export {
+  showNextImage,
+  showPreviousImage,
+  linkCircleWithClickedImage,
+};
