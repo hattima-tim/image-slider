ae11b65cd485a5442f90af52f1b4edf19e56a7ac
ridwan <irfanridwan20@gmail.com>
2021-06-27 23:38:51 +0600

    added some code and clearInterval function for reseting setInterval timer everytime user clicks on navigation dots or navigation buttons

--- src/index.js
+++ src/index.js
@@ -4,21 +4,28 @@ import {
   linkCircleWithClickedImage,
 } from './domManipulation';
 
+let myTimer = window.setInterval(showNextImage, 5000);
 const forwardButton = document.querySelector('#forward');
 const backwardButton = document.querySelector('#backward');
 const circles = document.querySelectorAll('.dot');
 
 forwardButton.addEventListener('click', () => {
   showNextImage();
+  clearInterval(myTimer);
+  myTimer = window.setInterval(showNextImage, 5000);
 });
 
 backwardButton.addEventListener('click', () => {
   showPreviousImage();
+  clearInterval(myTimer);
+  myTimer = window.setInterval(showNextImage, 5000);
 });
 
 circles.forEach((circle, index) => {
   circle.setAttribute('data-index', `${index}`);
   circle.addEventListener('click', (e) => {
     linkCircleWithClickedImage(e);
+    clearInterval(myTimer);
+    myTimer = window.setInterval(showNextImage, 5000);
   });
 });
