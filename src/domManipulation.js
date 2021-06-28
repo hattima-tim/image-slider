b35845c1e802fd1cffaa578631302a102bfcef6c
ridwan <irfanridwan20@gmail.com>
2021-06-28 21:03:57 +0600

    chnaged some varaible and function name for improving code readability

--- src/domManipulation.js
+++ src/domManipulation.js
@@ -1,71 +1,71 @@
 const images = document.querySelectorAll('img');
-const circles = document.querySelectorAll('.dot');
-circles[0].style.backgroundColor = 'red';
-let a = 0;
+const navigationDots = document.querySelectorAll('.dot');
+navigationDots[0].style.backgroundColor = 'red';
+let imageIndex = 0;
 
 const showNextImage = () => {
-  if (a === 3) {
-    a = -1;
+  if (imageIndex === 3) {
+    imageIndex = -1;
   }
-  a += 1;
+  imageIndex += 1;
 
-  switch (a) {
+  switch (imageIndex) {
     case 0:
       images[3].classList.replace('visible', 'hidden');
       images[0].classList.replace('hidden', 'visible');
-      circles[3].style.backgroundColor = '#bbb';
-      circles[0].style.backgroundColor = 'red';
+      navigationDots[3].style.backgroundColor = '#bbb';
+      navigationDots[0].style.backgroundColor = 'red';
       break;
     case 3:
       images[2].classList.replace('visible', 'hidden');
       images[3].classList.replace('hidden', 'visible');
-      circles[2].style.backgroundColor = '#bbb';
-      circles[3].style.backgroundColor = 'red';
+      navigationDots[2].style.backgroundColor = '#bbb';
+      navigationDots[3].style.backgroundColor = 'red';
       break;
     default:
-      images[a - 1].classList.replace('visible', 'hidden');
-      images[a].classList.replace('hidden', 'visible');
-      circles[a - 1].style.backgroundColor = '#bbb';
-      circles[a].style.backgroundColor = 'red';
+      images[imageIndex - 1].classList.replace('visible', 'hidden');
+      images[imageIndex].classList.replace('hidden', 'visible');
+      navigationDots[imageIndex - 1].style.backgroundColor = '#bbb';
+      navigationDots[imageIndex].style.backgroundColor = 'red';
   }
 };
 
 const showPreviousImage = () => {
-  a -= 1;
-  if (a < 0) {
-    a = 3;
+  imageIndex -= 1;
+  if (imageIndex < 0) {
+    imageIndex = 3;
   }
-  switch (a) {
+  switch (imageIndex) {
     case 0:
       images[1].classList.replace('visible', 'hidden');
       images[0].classList.replace('hidden', 'visible');
-      circles[1].style.backgroundColor = '#bbb';
-      circles[0].style.backgroundColor = 'red';
+      navigationDots[1].style.backgroundColor = '#bbb';
+      navigationDots[0].style.backgroundColor = 'red';
       break;
     case 3:
       images[0].classList.replace('visible', 'hidden');
       images[3].classList.replace('hidden', 'visible');
-      circles[0].style.backgroundColor = '#bbb';
-      circles[3].style.backgroundColor = 'red';
+      navigationDots[0].style.backgroundColor = '#bbb';
+      navigationDots[3].style.backgroundColor = 'red';
       break;
     default:
-      images[a + 1].classList.replace('visible', 'hidden');
-      images[a].classList.replace('hidden', 'visible');
-      circles[a + 1].style.backgroundColor = '#bbb';
-      circles[a].style.backgroundColor = 'red';
+      images[imageIndex + 1].classList.replace('visible', 'hidden');
+      images[imageIndex].classList.replace('hidden', 'visible');
+      navigationDots[imageIndex + 1].style.backgroundColor = '#bbb';
+      navigationDots[imageIndex].style.backgroundColor = 'red';
   }
 };
 
-const linkCircleWithClickedImage = (e) => {
-  images[a].classList.replace('visible', 'hidden');
+const linkCircleWithImage = (e) => {
+  images[imageIndex].classList.replace('visible', 'hidden');
   images[e.target.dataset.index].classList.replace('hidden', 'visible');
-  circles[a].style.backgroundColor = '#bbb';
+  navigationDots[imageIndex].style.backgroundColor = '#bbb';
   e.target.style.backgroundColor = 'red';
-  a = Number(e.target.dataset.index);
+  imageIndex = Number(e.target.dataset.index);
 };
 
 export {
   showNextImage,
   showPreviousImage,
-  linkCircleWithClickedImage,
+  linkCircleWithImage,
 };
--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 import {
   showNextImage,
   showPreviousImage,
-  linkCircleWithClickedImage,
+  linkCircleWithImage,
 } from './domManipulation';
 
-let myTimer = window.setInterval(showNextImage, 5000);
+let intervalTimer = window.setInterval(showNextImage, 5000);
 const forwardButton = document.querySelector('#forward');
 const backwardButton = document.querySelector('#backward');
-const circles = document.querySelectorAll('.dot');
+const navigationDots = document.querySelectorAll('.dot');
 
 forwardButton.addEventListener('click', () => {
   showNextImage();
-  clearInterval(myTimer);
-  myTimer = window.setInterval(showNextImage, 5000);
+  clearInterval(intervalTimer);
+  intervalTimer = window.setInterval(showNextImage, 5000);
 });
 
 backwardButton.addEventListener('click', () => {
   showPreviousImage();
-  clearInterval(myTimer);
-  myTimer = window.setInterval(showNextImage, 5000);
+  clearInterval(intervalTimer);
+  intervalTimer = window.setInterval(showNextImage, 5000);
 });
 
-circles.forEach((circle, index) => {
+navigationDots.forEach((circle, index) => {
   circle.setAttribute('data-index', `${index}`);
   circle.addEventListener('click', (e) => {
-    linkCircleWithClickedImage(e);
-    clearInterval(myTimer);
-    myTimer = window.setInterval(showNextImage, 5000);
+    linkCircleWithImage(e);
+    clearInterval(intervalTimer);
+    intervalTimer = window.setInterval(showNextImage, 5000);
   });
 });
